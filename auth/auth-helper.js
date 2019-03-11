585fda32a569789eef2c9d07e0f755ebcb7d6d8f
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-11 16:10:44 -0400

    Complete POST for auth/login and auth/register

--- auth/auth-helper.js
+++ auth/auth-helper.js
@@ -2,16 +2,27 @@ const db = require("../database/db-config");
 
 module.exports = {
     registerUser,
+    getUserByUsername,
     getUserById
 }
 
 async function registerUser(user) {
+
     const [id] = await db("users").insert(user);
+    
     const newUser = await getUserById(id);
 
     return newUser;
 }
 
+function getUserByUsername(username) {
+
+    return db("users").where("username", username).first();
+
+}
+
 function getUserById(id) {
+
     return db("users").where({id}).first();
+
 }
--- auth/auth-routes.js
+++ auth/auth-routes.js
@@ -9,16 +9,22 @@ const router = express.Router();
 router.post('/register', (req, res) => {
     let newUser = req.body;
 
-    if (!newUser.username || !newUser.password) {
+    if (!newUser.username || !newUser.password || !newUser.first_name) {
 
-        res.status(400).json({message: "Please provide a username and password to register."})
+        res.status(400).json({message: "Please provide a username, password, and first name to register."})
 
     } else {
 
         const hash = bcrypt.hashSync(newUser.password, 10);
         newUser.password = hash;
 
-        // database call to users table; insert new user and return new user's info (send 201 on success, 500 on server error)
+        Users.registerUser(newUser).then(user => {
+            console.log(user);
+            res.status(201).json(user);
+        }).catch(err => {
+            res.status(500).json(err);
+        })
+
     }
 });
 
@@ -27,23 +33,29 @@ router.post('/login', (req, res) => {
 
     if (username && password) {
 
-        // database call to users table by id -- name variable "user"
+        Users.getUserByUsername(username).then(user => {
 
-        if (user && bcrypt.compareSync(password, user.password)) {
+            if (user && bcrypt.compareSync(password, user.password)) {
 
-            const token = generateToken.generateToken(user);
-            res.status(200).json({message: `Welcome, ${user.username}`, token});
+                const token = generateToken.generateToken(user);
+                res.status(200).json({message: `Welcome, ${user.username}`, token});
+    
+            } else {
+    
+                res.status(404).json({message: "Invalid credentials."});
+    
+            }   
+        }).catch(err => {
 
-        } else {
+            res.status(500).json(err);
+        })
 
-            res.status(404).json({message: "Invalid credentials."});
 
-        }
     } else {
 
         res.status(401).json({message: "Please provide a username and password to log in."})
 
     }
-})
+});
 
 module.exports = router;
