afcef00b3f6dbdf3bdcc3e0644505e62b2a21c3a
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-11 18:06:43 -0400

    Add postgres local and deployment options to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,12 @@
+const localPgConnection = {
+  host: 'localhost',
+  database: 'postgres',
+  user: 'postgres',
+  password: process.env.LOCALPG_PW
+}
+
+const prodDbConnection = process.env.DATABASE_URL || localPgConnection;
+
 module.exports = {
 
   development: {
@@ -12,40 +21,36 @@ module.exports = {
     },
     seeds: {
       directory: './database/seeds',
-      tableName: 'db_seeds'
     }
   },
 
-  staging: {
-    client: 'postgresql',
+  testing: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './database/test.db3',
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: prodDbConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
   }
 
 };
