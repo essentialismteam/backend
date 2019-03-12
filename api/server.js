650e4a3d53ab967a334bb9303884c518bc4e1ae9
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-12 12:12:16 -0400

    Remove SQLite errors from auth routes, add users endpoint

--- api/server.js
+++ api/server.js
@@ -4,13 +4,15 @@ const helmet = require('helmet');
 
 const server = express();
 
-const authRoutes = require('../auth/auth-routes')
+const authRoutes = require('../auth/auth-routes');
+const userRoutes = require('../user/user-routes');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 server.use('/auth', authRoutes);
+server.use('/users', userRoutes)
 
 server.get('/', (req, res) => {
     res.status(200).send("Welcome to Essentialism.");
