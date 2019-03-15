1b37dcb2b0491d2796ee893dfb0ffa23b536f182
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-03-15 10:29:29 -0400

    Remove testing env script, add server, auth and user tests, remove middleware for tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,26 +1,31 @@
 const request = require("supertest");
 const server = require("./server");
+const db = require('../database/db-config');
+
+afterEach(async () => {
+  await db("users").truncate();
+})
 
 describe("server.js", () => {
-    it.skip("should set testing environment", () => {
-        expect(process.env.DB_ENV).toBe("development");
-      });
+    // it("should set testing environment", () => {
+    //     expect(db.dbEnv).toBe("development");
+    //   });
 
       
   describe("GET /", () => {
-    it.skip("responds 200 OK", async () => {
+    it("responds 200 OK", async () => {
       const res = await request(server).get("/");
 
       expect(res.status).toEqual(200);
     });
 
-    it.skip("returns a JSON object", async () => {
+    it("returns a JSON object", async () => {
       const res = await request(server).get("/");
 
       expect(res.type).toEqual("application/json");
     });
 
-    it.skip("returns expected message", async () => {
+    it("returns expected message", async () => {
       const json = { message: "Welcome to the Essentialism API." };
       const res = await request(server).get("/");
 
---
+++ user/user.spec.js
@@ -0,0 +1,76 @@
+const request = require("supertest");
+const server = require("../api/server");
+const bcrypt = require("bcryptjs");
+const db = require("../database/db-config");
+
+// Attempted to send auth token with all requests, token returns undefined
+
+// let token;
+// beforeAll(done => {
+//   request(server)
+//     .post("/auth/login")
+//     .send({ username: "admin", password: bcrypt.hashSync("password", 10) })
+//     .end((err, res) => {
+//       token = res.body.token;
+//       done();
+//     });
+// });
+
+afterEach(async () => {
+  await db("users").truncate();
+  await db("values").truncate();
+  await db("journal").truncate();
+  await db("projects").truncate();
+  await db("user_values").truncate();
+});
+
+describe("user-routes.js", () => {
+  describe("GET /users/:id", () => {
+    it("responds 200 OK", async () => {
+      const res = await request(server)
+        .get("/users/1")
+        // .set("Authorization", `Bearer ${token}`);
+
+      expect(res.status).toEqual(200);
+    });
+
+    it("returns a JSON object", async () => {
+      const res = await request(server).get("/users/1");
+
+      expect(res.type).toEqual("application/json");
+    });
+  });
+
+//   describe("PUT /users/:id", () => {
+//     it("responds 200 OK", async () => {
+
+//         const res = await request(server)
+//           .put("/users/1").send({first_name: "Bob", last_name: "Hope"})
+//           // .set("Authorization", `Bearer ${token}`);
+  
+//         expect(res.status).toEqual(200);
+//       });
+  
+//       it.skip("returns a JSON object", async () => {
+//         const res = await request(server).get("/users/1");
+  
+//         expect(res.type).toEqual("application/json");
+//       });
+//   })
+
+describe("/users/:id/journal", () => {
+    describe("POST journal", () => {
+        it("returns 201", async () => {
+            const res = await request(server).post("/users/1/journal").send({journal_entry: "Text"})
+
+            expect(res.status).toEqual(201);
+        });
+
+        it("returns a JSON object", async () => {
+            const res = await request(server).post("/users/1/journal").send({journal_entry: "Test"})
+
+            expect(res.type).toEqual("application/json");
+        })
+    })
+})
+});
